3378deabead4842d916b864c111548b7e2160c16
Just-Mario <mario.nyangintaka@gmail.com>
2020-12-01 21:44:28 +0200

    initial commit

---
+++ dist/main.js
@@ -0,0 +1,23 @@
+
+
+AOS.init({
+    easing: 'ease',
+    duratio: '1800',
+    once: true
+
+});
+
+var menuBtn = document.getElementById("menuBtn");
+var sideNav = document.getElementById("sideNav");
+
+sideNav.style.right = "-250px";
+
+menuBtn.onclick = function () {
+    if (sideNav.style.right == "-250px") {
+        sideNav.style.right = "0";
+    }
+    else {
+        sideNav.style.right = "-250px";
+    }
+
+}
